ae4717f134d7673210832a602a5109607c9eaa1e
Olson Ngula <117510430+ngush67@users.noreply.github.com>
2025-02-12 13:35:54 +0300

    Update 6-sum_mixed_list.py

--- 0x00-python_variable_annotations/6-sum_mixed_list.py
+++ 0x00-python_variable_annotations/6-sum_mixed_list.py
@@ -2,7 +2,7 @@
 '''Task 6's module.
 '''
 from typing import List, Union
-
+ 
 
 def sum_mixed_list(mxd_lst: List[Union[int, float]]) -> float:
     '''Computes the sum of a list of integers and floating-point numbers.
